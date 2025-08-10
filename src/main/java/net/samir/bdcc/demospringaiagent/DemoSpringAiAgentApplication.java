df1403dcf649331725c1d1cd4c0a3b55c4eafffc
Samireddebbarhi <samireddebbarhi29@gmail.com>
2025-08-10 22:18:36 +0100

    Adding Agent Controller

---
+++ src/main/java/net/samir/bdcc/demospringaiagent/DemoSpringAiAgentApplication.java
@@ -0,0 +1,13 @@
+package net.samir.bdcc.demospringaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringAiAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/samir/bdcc/demospringaiagent/DemoSpringAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package net.samir.bdcc.demospringaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
